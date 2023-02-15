9ba5966ee2ee5e918d5fb266c81b7c708d4eeca7
Alifian Erga Iswardhana <114549282+AlfErga04@users.noreply.github.com>
2023-02-15 20:03:04 +0700

    Add files via upload

---
+++ Pertemuan minggu 2/Coding_2.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <conio.h>
+using namespace std;
+
+int main() 
+{
+    int x;
+    int *px; //pointer ke variabel
+    int **ppx; //pointer ke pointer
+    x = 175;
+    px = &x;
+    ppx = &px;
+    cout << "Nilai x = " << x << endl;
+    cout << "Nilai px = " << *px << endl; 
+    cout << "Nilai ppx = " << **ppx << endl;
+
+    getch();
+    return 0;
+}
---
+++ Pertemuan minggu 2/Coding_6.cpp
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main()
+{
+    char nama[] = "aliong";
+    char *p;
+    //cara 1 >> Langsung menunjuk nama array
+    p = nama;
+    while (*p != NULL)
+    {
+        printf("%c", *p);
+        p++;
+    }
+    printf("\n");
+    //cara 2 >> secara eksplisit menunjuk ke elemen pertama array
+    p=&nama[0];
+    while (*p != NULL)
+    {
+        printf("%c", *p);
+        p++;
+    }
+    printf("\n");
+    return 0;
+}
